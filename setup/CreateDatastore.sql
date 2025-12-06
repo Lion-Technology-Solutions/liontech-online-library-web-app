409a3e24f45577fe59c1208667a74cf03c55220b
bright <terrybright80@gmail.com>
2025-12-06 08:51:10 +0100

    building sceipted

--- setup/CreateDatastore.sql
+++ setup/CreateDatastore.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE if not exists liontechbookstore;
+CREATE DATABASE if not exists onlinebookstore;
 
-\c liontechbookstore
+\c onlinebookstore
 
 CREATE TABLE if not exists books 
   ( 
